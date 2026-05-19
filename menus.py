62c3e20a315b6350b022f7551d22f8898edb7529
ZixSec <156583439+ZIXP01@users.noreply.github.com>
2026-05-19 14:54:03 +0300

    Update menus.py

--- menus.py
+++ menus.py
@@ -1,2 +1,11 @@
 # menus.py
 # All client menus
+
+def main_menu():
+
+    print("\n========== MAIN MENU ==========")
+    print("1. Browse recipes")
+    print("2. Reference lists")
+    print("3. Quit")
+
+return input("Choose option: ")
