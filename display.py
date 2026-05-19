11db60a500499c114780990fae83f31527a9baa6
ZixSec <156583439+ZIXP01@users.noreply.github.com>
2026-05-19 14:58:37 +0300

    Update display.py

--- display.py
+++ display.py
@@ -5,8 +5,16 @@
 def display_recipe_list(data):
 
     print("\n========== RECIPES ==========")
-    meals = data.get("meals", [])
+    meals = data.get("meals", []) 
 
     if not meals:
         print("No recipes found.")
         return
+
+for meal in meals:
+
+        print(f"Meal ID: {meal.get('idMeal')}")
+        print(f"Recipe Name: {meal.get('strMeal')}")
+        print(f"Thumbnail: {meal.get('strMealThumb')}")
+        print("------------------------------")
+
