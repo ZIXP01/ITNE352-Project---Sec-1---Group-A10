01b674b0fe850564e853b1316b99532a1f3a00bb
rawanread <vz8jr45945@privaterelay.appleid.com>
2026-05-19 14:56:54 +0300

    Update cache.py

--- cache.py
+++ cache.py
@@ -1,5 +1,8 @@
 import requests
 import json
 class ReferenceCache:
-  def __init__(self):
-    """  Store reference lists in memory """
+def __init__(self):
+ """  Store reference lists in memory """
+self.categories = []
+self.areas = []
+self.ingredients = []
