e5ee9c91972fd25f07bdc725d8542bdd01f47116
ZixSec <156583439+ZIXP01@users.noreply.github.com>
2026-05-19 14:35:36 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,2 +1,13 @@
 # client.py
 # Main client script
+import socket
+import json
+
+from menus import main_menu, recipes_menu, reference_menu
+from display import (
+    display_recipe_list,
+    display_recipe_details,
+    display_categories,
+    display_areas,
+    display_ingredients
+)
