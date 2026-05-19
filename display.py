ba9c7553ea83b45e51f5dcc5b60ad8d026f8484f
ZixSec <156583439+ZIXP01@users.noreply.github.com>
2026-05-19 14:57:20 +0300

    Update display.py

--- display.py
+++ display.py
@@ -1,3 +1,12 @@
 # display.py
 # Functions for displaying responses
 # Display recipe list
+
+def display_recipe_list(data):
+
+    print("\n========== RECIPES ==========")
+    meals = data.get("meals", [])
+
+    if not meals:
+        print("No recipes found.")
+        return
