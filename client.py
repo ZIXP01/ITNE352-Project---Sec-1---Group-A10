af61a0b2d1a212937b14552f309624980e182b08
ZixSec <156583439+ZIXP01@users.noreply.github.com>
2026-05-19 14:44:24 +0300

    Update client.py

--- client.py
+++ client.py
@@ -96,5 +96,11 @@ display_recipe_details(details)
 response = receive_response(client_socket)
 display_recipe_list(response)
 meal_id = input("Enter Meal ID for full details: ")
+ send_request(client_socket, {
+                        "type": "meal_details",
+                        "meal_id": meal_id
+                    })
+details = receive_response(client_socket)
+display_recipe_details(details)
 
 
